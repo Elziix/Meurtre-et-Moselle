56016ca69cd7cc1ec56a5744e217257fe34ef53c
Killian Rattier <killian.rattier@gmail.com>
2023-02-28 12:03:19 +0100

    Ajout des composants background + explanation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { BackgroundComponent } from './components/background/background.component';
+import { ExplanationComponent } from './components/explanation/explanation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    BackgroundComponent,
+    ExplanationComponent
   ],
   imports: [
     BrowserModule,

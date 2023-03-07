e23a1a659befd1c1ecfa51e8a95c3080699fe98f
Killian Rattier <killian.rattier@gmail.com>
2023-03-07 12:00:25 +0100

    add pin on click (bug)

--- src/app/components/map-box/map-box.component.ts
+++ src/app/components/map-box/map-box.component.ts
@@ -17,27 +17,47 @@ export class MapBoxComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    
+
     (mapboxgl as typeof mapboxgl).accessToken = 'pk.eyJ1IjoiZWx6aXgiLCJhIjoiY2xleTB1cGdxMDBmbTN4bDUxMmR3ZWU4bCJ9.-UEOn7npmVEpi3mbP5TOUg';
-      this.map = new mapboxgl.Map({
-        container: 'map',
-        style: this.style,
-        zoom: 5,
-        center: [this.lng, this.lat]
-    });    
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: this.style,
+      zoom: 5,
+      center: [this.lng, this.lat]
+    });
     // Add map controls
     this.map.addControl(new mapboxgl.NavigationControl());
 
-     
-    // Create a default Marker and add it to the map.
-    const marker1 = new mapboxgl.Marker()
-    .setLngLat([12.554729, 55.70651])
-    .addTo(this.map);
-    
-    // Create a default Marker, colored black, rotated 45 degrees.
-    const marker2 = new mapboxgl.Marker({ color: 'black', rotation: 45 })
-    .setLngLat([12.65147, 55.608166])
-    .addTo(this.map);
-  }
+    var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-79.4512, 43.6568],
+      zoom: 13
+    });
 
+    var marker = new mapboxgl.Marker();
+
+    function add_marker(event: { lngLat: mapboxgl.LngLat; }) {
+      var coordinates = event.lngLat;
+      console.log('Lng:', coordinates.lng, 'Lat:', coordinates.lat);
+      marker.setLngLat(coordinates).addTo(map);
+      console.log('Marker added to map');
+      marker.setLngLat([0, 0]).addTo(map);
+      console.log('Marker auto added to map');
+    }
+
+    map.on('dblclick', add_marker);
+    map.off('mousemove', add_marker);
+
+    // Create a function to unbind the `mousemove` event.
+    function onUp(e : { lngLat: mapboxgl.LngLat }) {
+      console.log(`The final coordinates are: ${e.lngLat}`);
+      map.off('mousemove', add_marker);
+    }
+
+
+  }
 }
+
+
+

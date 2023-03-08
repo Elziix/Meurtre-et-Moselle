cc4c13232ad318a9ef0d3be653558847ab39615b
Killian Rattier <killian.rattier@gmail.com>
2023-03-08 16:07:23 +0100

    add js functions getCityData + getDepartment with geo api

--- src/app/components/map-box/map-box.component.ts
+++ src/app/components/map-box/map-box.component.ts
@@ -26,17 +26,67 @@ export class MapBoxComponent implements OnInit {
       center: [this.lng, this.lat]
     });
 
+    const apiUrl = "https://geo.api.gouv.fr/communes"; // URL de l'API Geo de l'API Gouv
+    const apiUrlDep = "https://geo.api.gouv.fr/departements";
+
+    async function getCityData(lati: number, longi: number) {
+      const url = `${apiUrl}?lat=${lati}&lon=${longi}&fields=nom,code,codesPostaux,siren,codeEpci,codeDepartement,codeRegion,population&format=json`;
+    
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+    
+        const cityData = data[0];
+    
+        // Call getDepartment with the department code from cityData
+        getDepartment(cityData.codeDepartement).then(departmentData => {
+          console.log(departmentData); // Display department data
+        });
+    
+        return cityData;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    async function getDepartment(dep: number) {
+      const url = `${apiUrlDep}?code=${dep}&fields=nom,code,codeRegion`
+
+      try {
+        // Effectuer la requête à l'API
+        const response = await fetch(url);
+
+        // Convertir la réponse en objet JSON
+        const data = await response.json();
+
+        // Renvoyer les données de la première ville trouvée
+        return data[0];
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
     var marker = new mapboxgl.Marker();
 
     function add_marker(event: { lngLat: mapboxgl.LngLat; }) {
       const coordinates = event.lngLat;
       console.log('Lng:', coordinates.lng, 'Lat:', coordinates.lat);
       marker.setLngLat(coordinates).addTo(map);
+
+      // Exemple d'utilisation de la fonction getCityData() avec la ville de Paris
+      getCityData(coordinates.lat, coordinates.lng).then(cityData => {
+        console.log(cityData); // Affiche les informations de la ville de Paris
+      });
+
     }
 
     //map.dragRotate.disable();
 
     map.on('click', add_marker);
+
+
+
+
 
   }
 }

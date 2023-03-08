f2ac833380299fc903c7ba2b30e0e840700337b6
Killian Rattier <killian.rattier@gmail.com>
2023-03-08 14:07:12 +0100

    fonction add marker fonctionnelle

--- src/app/components/map-box/map-box.component.ts
+++ src/app/components/map-box/map-box.component.ts
@@ -11,50 +11,32 @@ export class MapBoxComponent implements OnInit {
 
   map?: mapboxgl.Map;
   style = 'mapbox://styles/mapbox/streets-v11';
-  lat = 37.75;
-  lng = -122.41;
+  lat = 55.665957;
+  lng = 12.550343;
 
   constructor() { }
 
   ngOnInit(): void {
 
     (mapboxgl as typeof mapboxgl).accessToken = 'pk.eyJ1IjoiZWx6aXgiLCJhIjoiY2xleTB1cGdxMDBmbTN4bDUxMmR3ZWU4bCJ9.-UEOn7npmVEpi3mbP5TOUg';
-    this.map = new mapboxgl.Map({
+    const map = new mapboxgl.Map({
       container: 'map',
       style: this.style,
-      zoom: 5,
+      zoom: 8,
       center: [this.lng, this.lat]
-    });
-    // Add map controls
-    this.map.addControl(new mapboxgl.NavigationControl());
-
-    var map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-79.4512, 43.6568],
-      zoom: 13
     });
 
     var marker = new mapboxgl.Marker();
 
-    function add_marker(event: { lngLat: mapboxgl.LngLat; }) {
-      var coordinates = event.lngLat;
+    function add_marker(event: { lngLat: mapboxgl.LngLat; carte: mapboxgl.Map;}) {
+      const coordinates = event.lngLat;
       console.log('Lng:', coordinates.lng, 'Lat:', coordinates.lat);
       marker.setLngLat(coordinates).addTo(map);
-      console.log('Marker added to map');
-      marker.setLngLat([0, 0]).addTo(map);
-      console.log('Marker auto added to map');
     }
 
-    map.on('dblclick', add_marker);
-    map.off('mousemove', add_marker);
+    map.dragRotate.disable();
 
-    // Create a function to unbind the `mousemove` event.
-    function onUp(e : { lngLat: mapboxgl.LngLat }) {
-      console.log(`The final coordinates are: ${e.lngLat}`);
-      map.off('mousemove', add_marker);
-    }
-
+    map.on('click', add_marker);
 
   }
 }

8d34e3047200a3f8a52766272a036b5d7e56ce5e
Killian Rattier <killian.rattier@gmail.com>
2023-03-08 16:32:09 +0100

    add getCityData global

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,43 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'dashfront';
 }
+
+const apiUrl = "https://geo.api.gouv.fr/communes"; // URL de l'API Geo de l'API Gouv
+    const apiUrlDep = "https://geo.api.gouv.fr/departements";
+
+    export async function getCityData(lati: number, longi: number) {
+      const url = `${apiUrl}?lat=${lati}&lon=${longi}&fields=nom,code,codesPostaux,siren,codeEpci,codeDepartement,codeRegion,population&format=json`;
+    
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+    
+        const cityData = data[0];
+    
+        // Call getDepartment with the department code from cityData
+        getDepartment(cityData.codeDepartement).then(departmentData => {
+          console.log(departmentData); // Display department data
+        });
+    
+        return cityData;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    async function getDepartment(dep: number) {
+      const url = `${apiUrlDep}?code=${dep}&fields=nom,code,codeRegion`
+
+      try {
+        // Effectuer la requête à l'API
+        const response = await fetch(url);
+
+        // Convertir la réponse en objet JSON
+        const data = await response.json();
+
+        // Renvoyer les données de la première ville trouvée
+        return data[0];
+      } catch (error) {
+        console.error(error);
+      }
+    }
--- src/app/components/map-box/map-box.component.ts
+++ src/app/components/map-box/map-box.component.ts
@@ -1,6 +1,7 @@
 import { environment } from '../../../environments/environment';
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
+import {getCityData} from "../../app.component"
 
 @Component({
   selector: 'app-map-box',
@@ -25,46 +26,6 @@ export class MapBoxComponent implements OnInit {
       zoom: 5,
       center: [this.lng, this.lat]
     });
-
-    const apiUrl = "https://geo.api.gouv.fr/communes"; // URL de l'API Geo de l'API Gouv
-    const apiUrlDep = "https://geo.api.gouv.fr/departements";
-
-    async function getCityData(lati: number, longi: number) {
-      const url = `${apiUrl}?lat=${lati}&lon=${longi}&fields=nom,code,codesPostaux,siren,codeEpci,codeDepartement,codeRegion,population&format=json`;
-    
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-    
-        const cityData = data[0];
-    
-        // Call getDepartment with the department code from cityData
-        getDepartment(cityData.codeDepartement).then(departmentData => {
-          console.log(departmentData); // Display department data
-        });
-    
-        return cityData;
-      } catch (error) {
-        console.error(error);
-      }
-    }
-
-    async function getDepartment(dep: number) {
-      const url = `${apiUrlDep}?code=${dep}&fields=nom,code,codeRegion`
-
-      try {
-        // Effectuer la requête à l'API
-        const response = await fetch(url);
-
-        // Convertir la réponse en objet JSON
-        const data = await response.json();
-
-        // Renvoyer les données de la première ville trouvée
-        return data[0];
-      } catch (error) {
-        console.error(error);
-      }
-    }
 
     var marker = new mapboxgl.Marker();
 

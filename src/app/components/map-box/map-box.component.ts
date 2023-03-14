62499ef258e9c7a4373d3a4f03ae5f6a440bfd32
Killian Rattier <killian.rattier@gmail.com>
2023-03-14 10:26:27 +0100

    add documentation

--- src/app/components/map-box/map-box.component.ts
+++ src/app/components/map-box/map-box.component.ts
@@ -1,72 +1,135 @@
-import { environment } from '../../../environments/environment';
+/**
+ * Module de composant pour l'affichage de la carte MapBox.
+ * @remarks
+ * Ce composant est chargé d'afficher la carte MapBox sur la page web.
+ * Il permet également d'ajouter des marqueurs à la carte et de rechercher les coordonnées d'une ville.
+ * @packageDocumentation
+ */
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 import { getCityData, getLatitudeLongitude } from "../../app.component"
 
+/** 
+ * Composant pour l'affichage de la carte MapBox.
+ */
 @Component({
   selector: 'app-map-box',
   templateUrl: './map-box.component.html',
   styleUrls: ['./map-box.component.css']
 })
-
 export class MapBoxComponent implements OnInit {
 
+  /**
+   * Carte MapBox.
+   */
   map?: mapboxgl.Map;
+
+  /**
+   * Style de la carte MapBox.
+   */
   style = 'mapbox://styles/mapbox/streets-v12';
+
+  /**
+   * Latitude de la carte.
+   */
   lat = 47;
+
+  /**
+   * Longitude de la carte.
+   */
   lng = 2.5;
- 
+
+  /**
+   *Indique si la carte est initialisée ou non.
+   */
   isMapInitialized = false;
+
+  /**
+   * Marqueur sur la carte.
+   */
   marker?: mapboxgl.Marker;
+
+  /**
+    * Constructeur du composant MapBox.
+    */
   constructor() { }
 
+  /**
+   * Initialise le composant MapBox.
+   * @remarks
+   * Cette fonction est appelée lors de l'initialisation du composant.
+   * Elle charge la carte MapBox et gère l'ajout de nouveaux marqueurs.
+   */
   ngOnInit(): void {
+
+    // Affectation du token
     (mapboxgl as typeof mapboxgl).accessToken = 'pk.eyJ1IjoiZWx6aXgiLCJhIjoiY2xleTB1cGdxMDBmbTN4bDUxMmR3ZWU4bCJ9.-UEOn7npmVEpi3mbP5TOUg';
+
+    // Initialisation de la map
     const map = new mapboxgl.Map({
       container: 'map',
       style: this.style,
       zoom: 5,
       center: [this.lng, this.lat]
     });
+
+    // Initialisation du marker
     this.marker = new mapboxgl.Marker();
 
-    //map.dragRotate.disable();
+    // Affectation de la fonction add_marker lorsqu'un event click est lancé sur la map
     map.on('click', this.add_marker.bind(this));
-    this.map = map;
 
+    // map sera reconnu en tant que this.map
+    this.map = map;
   }
 
+  /**
+   * Cette fonction ajoute un marqueur sur la map et effectue un zoom sur le marqueur.
+   * Elle utilise la fonction getCityData pour récupérer les informations de la ville correspondante.
+   * @param {object} event - L'événement click de la map.
+   */
   add_marker(event: { lngLat: mapboxgl.LngLat; }) {
-    console.log("entre add_marker");
+
+    // Initialise des coordonées à l'endroit où a été lancé le click event
     const coordinates = event.lngLat;
+
+    // Affichage des coordonées dans la console pour vérification
     console.log('Lng:', coordinates.lng, 'Lat:', coordinates.lat);
-    console.log("ajout du marker")
-    if(this.map!){
-      console.log("map exists");
-    }
+
+    // Si la map et le marker sont reconnus (donc bien initialisés)
     if (this.map && this.marker) {
+      // On chnage les coordonées du marker puis on l'ajoute à la map
       this.marker.setLngLat(coordinates).addTo(this.map);
+      // On zoom sur l'endroit marqué
       this.map.flyTo({ center: coordinates, zoom: 8 });
     }
-    console.log("marker ajouté");
-
-    // Exemple d'utilisation de la fonction getCityData() avec la ville de Paris
+  
+    // Appelle la fonction getCityData nous donnant les informations concernant les coordonées clickées
     getCityData(coordinates.lat, coordinates.lng).then(cityData => {
-      console.log(cityData); // Affiche les informations de la ville de Paris
+      console.log(cityData);
     });
   }
 
-  // Cette fonction prends une ville en paramètre, recherche les coordonnées de cette ville et ajoute un markeur à cet endroit de la map
+  /**
+   * Cette fonction prend une ville en paramètre, recherche les coordonnées de cette ville et ajoute un marqueur à cet endroit de la map.
+   * Elle utilise la fonction getLatitudeLongitude pour récupérer les coordonnées de la ville.
+   * @param {string} city - Le nom de la ville à rechercher.
+   */
   searchCity(city: string) {
     console.log('entre dans searchCity : ' + city);
+    // On recupère les coordonnées de la ville
     getLatitudeLongitude(city)
       .then((coords) => {
         console.log('Coordonnées de la ville', coords);
+        // Si la map et les coordonnées existent
         if (this.map && coords) {
+          // On cahnge les coordonées du marker puis on l'ajoute à la map
           this.marker?.setLngLat(coords).addTo(this.map);
+          // On zoom sur l'endroit marqué
           this.map.flyTo({ center: coords, zoom: 8 });
         }
       })
+      // Si une erreur est rencontrée on l'affiche dans la console
       .catch((err) => {
         console.error(
           'Erreur lors de la récupération des coordonnées de la ville',
@@ -74,11 +137,5 @@ export class MapBoxComponent implements OnInit {
         );
       });
   }
-  
-  
-  
-  
-  
-  
 }
 
--- src/app/components/searchbar/searchbar.component.ts
+++ src/app/components/searchbar/searchbar.component.ts
@@ -7,18 +7,25 @@ import { getLatitudeLongitude } from '../../app.component';
   styleUrls: ['./searchbar.component.css'],
 })
 export class SearchbarComponent implements OnInit {
+
+  // citySelected est considéré comme un evenement de type String
   @Output() citySelected = new EventEmitter<string>();
 
+  // Le nom de la commune recherchée
   nomCommune: string = '';
+
+  // Les coordonées de cette commune
   coords: { lat: number, lng: number } | undefined = undefined;
 
   constructor() {}
 
   ngOnInit(): void {}
 
+  // Fonction asynchrone appelant searchCity avec le nom de la commune
   async search() {
     const coords = await getLatitudeLongitude(this.nomCommune);
     if (coords) {
+      // On envoie l'output à map-box.component
       this.citySelected.emit(this.nomCommune);
     }
   }

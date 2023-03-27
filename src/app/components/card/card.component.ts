fe67932bc76a1d337d475f4d9648b7e5012f1bdc
Cyprien Pennachi <cyprienpennachi@gmail.com>
2023-03-27 17:53:01 +0200

    add doc card

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,8 +1,17 @@
+/**
+ * Module de composant pour l'affichage des affaires.
+ * @remarks
+ * Ce composant est chargé d'afficher les affaires liées à un département sur la page web.
+ * @packageDocumentation
+ */
 import { ChangeDetectorRef, Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChange, SimpleChanges, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+/**
+ * Interface nous permettant de récupérer les informations liées à une affaire via recherche dans le JSON
+ */
 interface Tueur {
   date: string;
   departement: string;
@@ -19,7 +28,14 @@ interface Tueur {
 })
 
 export class CardComponent implements OnInit, OnChanges {
+  /**
+   * La liste des affaires liées à la recherche
+  */
   listeAffaires: Array<Tueur>;
+
+  /**
+   * Récupère le département entré dans la searchbar
+   */
   @Input() departement: string;
   
   constructor(private http: HttpClient, private cdr: ChangeDetectorRef) { this.listeAffaires = [], this.departement = "" }
@@ -28,6 +44,10 @@ export class CardComponent implements OnInit, OnChanges {
 
   }
 
+  /**
+   * Au changement detecté, on parcourt le JSON et on recherche les affaires liées au département stocké actuellement
+   * @param changes le changement à détecter
+   */
   ngOnChanges(changes: SimpleChanges) {
     const element = document.getElementById('scroll');
     if (changes['departement'] && !changes['departement'].firstChange) {
@@ -52,6 +72,11 @@ export class CardComponent implements OnInit, OnChanges {
     }
   }
 
+  /**
+   * Recherche les affaires liées à un département
+   * @param departement le département dans lequel sesont déroulées les affaires recherchées
+   * @returns la liste des affaires
+   */
   getAffaires(departement: string): Observable<Array<Tueur>> {
     const url = './assets/data.json';
     console.log('Entre dans getAffaires');
@@ -90,17 +115,4 @@ export class CardComponent implements OnInit, OnChanges {
       })
     );
   }
-
-  getRows(): any[] {
-    const rows = [];
-    const numCols = 3;
-    for (let i = 0; i < this.listeAffaires.length; i += numCols) {
-      const row = this.listeAffaires.slice(i, i + numCols);
-      rows.push(row);
-    }
-    return rows;
-  }
-
-  
-  
 }

7f4bcb0dad1aa44050d5041b430c8a2088cc5c85
Cyprien Pennachi <cyprienpennachi@gmail.com>
2023-03-27 17:56:50 +0200

    add doc explanation

--- src/app/components/explanation/explanation.component.ts
+++ src/app/components/explanation/explanation.component.ts
@@ -1,3 +1,9 @@
+/**
+ * Module de composant pour l'affichage des indications.
+ * @remarks
+ * Ce composant est chargé d'afficher les indications utiles à la présentation et l'utilisation de notre site.
+ * @packageDocumentation
+ */
 import { Component, OnInit } from '@angular/core';
 
 @Component({
